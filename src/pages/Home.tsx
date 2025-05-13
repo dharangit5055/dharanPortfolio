4ea30605b410ef1452db8129bb9b28e8c77a63e7
Dharaneesh N A <dharaneeshna@gmail.com>
2025-05-13 20:54:02 +0530

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,7 +25,7 @@ const Home: React.FC = () => {
             <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-gray-900 dark:text-white">
               Hello, I'm{" "}
               <span className="text-blue-600 dark:text-blue-400">
-                Dharaneesh test
+                Dharaneesh
               </span>
             </h1>
             <p className="mt-6 text-xl text-gray-600 dark:text-gray-300">
